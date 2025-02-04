89e54e67a653b39869bd0dfe63b77cfd0c5f0122
antonio-wachave <anthonypaulino32@gmail.com>
2025-02-04 15:39:06 +0200

    update service add, update, delete

--- src/main/java/com/wachave/employees/services/EmployeeService.java
+++ src/main/java/com/wachave/employees/services/EmployeeService.java
@@ -6,5 +6,15 @@ import java.util.List;
 
 public interface EmployeeService {
 
+    //get a list of employee
     List<Employee> findAll();
+
+    //get an employee
+    Employee findById(int employeeId);
+
+    //save an employee
+    Employee saveEmployee(Employee employee);
+
+    //delete an employee
+    void deleteById(int employeeId);
 }
--- src/main/java/com/wachave/employees/services/EmployeeServiceImpl.java
+++ src/main/java/com/wachave/employees/services/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.wachave.employees.services;
 
 import com.wachave.employees.dao.EmployeeDAO;
 import com.wachave.employees.entities.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +11,7 @@ import java.util.List;
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
 
-    private EmployeeDAO employeeDAO;
+    private final EmployeeDAO employeeDAO;
 
     @Autowired
     public EmployeeServiceImpl(EmployeeDAO theEmployeeDAO){
@@ -21,4 +22,23 @@ public class EmployeeServiceImpl implements EmployeeService{
     public List<Employee> findAll() {
         return this.employeeDAO.findAll();
     }
+
+    @Override
+    public Employee findById(int employeeId) {
+        return this.employeeDAO.findById(employeeId);
+    }
+
+    @Transactional
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return this.employeeDAO.saveEmployee(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int employeeId) {
+        this.employeeDAO.deleteById(employeeId);
+    }
+
+
 }
